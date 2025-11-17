9dab3619c75e53a747d4bac93f5c42ea88311ae6
hongwei <hongwei@ecquaria.com>
2025-11-17 15:24:36 +0800

    修改服务器地址

--- vite.config.ts
+++ vite.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
     cors: true,
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8080',
+        target: 'http://127.0.0.1:8081',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '/ecqee'),
         configure: (proxy, _options) => {
